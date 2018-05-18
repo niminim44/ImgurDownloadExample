fcb1c3aedef35d9c65d0ffb1b12acef4ced3b701
niminim <niminim44@gmail.com>
2018-05-18 11:57:31 +0300

    get the registration information ˆ

---
+++ app/src/main/java/com/postpc/nimrod/imgurdownloadexample/network/ImgurConstants.java
@@ -0,0 +1,8 @@
+package com.postpc.nimrod.imgurdownloadexample.network;
+
+public class ImgurConstants {
+
+    public static final String CLIENT_ID = "096f85a9edc2c7c";
+    public static final String SECRET = "77891f5b81bf4bdf586c2547037f5a21df8546be";
+
+}
